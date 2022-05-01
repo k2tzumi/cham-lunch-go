da9eb6ff85aa761ec1456aeb8a9c858f31a321c4
k2tzumi <katzumi+github@gmail.com>
2022-05-01 11:15:16 +0900

    ♻️ テストケース名に名前をつけてわかりやすくした

--- workspace/src/jma/jma_test.go
+++ workspace/src/jma/jma_test.go
@@ -9,16 +9,19 @@ import (
 
 func TestFindOfficeCode(t *testing.T) {
 	patterns := []struct {
+		name          string
 		cityName      string
 		expected      string
 		expectedError bool
 	}{
-		{"磐田市", "220000", false},
-		{"豊田市", "230000", false},
-		{"最上", "060000", false},
-		{"東葛飾", "120000", false},
-		{"奄美", "460040", false},
-		{"unknown", "", true},
+		{"Class20で見つかった場合", "磐田市", "220000", false},
+		{"Class20で同一地域で見つかった場合", "浜松市", "220000", false},
+		{"Class20で別地域で見つかった場合", "豊田市", "230000", false},
+		{"Class15で見つかった場合", "宗谷", "011000", false},
+		{"Class10で見つかった場合", "伊豆諸島", "130000", false},
+		{"前方一致で見つかること", "奄美", "460040", false},
+		{"完全一致で見つかり、結果は同じになること", "奄美地方", "460040", false},
+		{"存在しない場合はerrorが返却されること", "unknown", "", true},
 	}
 
 	f, err := os.Open("../../resources/area.json")
@@ -32,14 +35,14 @@ func TestFindOfficeCode(t *testing.T) {
 	area := &Area{}
 	json.Unmarshal(byteArray, &area)
 
-	for idx, pattern := range patterns {
+	for _, pattern := range patterns {
 		actual, actualError := FindOfficeCode(area, pattern.cityName)
 		if pattern.expected != actual {
-			t.Errorf("pattern %d: want %s, actual %s", idx, pattern.expected, actual)
+			t.Errorf("pattern %s: want %s, actual %s", pattern.name, pattern.expected, actual)
 		}
 		// FIXME: Check for strict errors
 		if (actualError == nil) == pattern.expectedError {
-			t.Errorf("pattern %d: want %t, actual %s", idx, pattern.expectedError, actualError)
+			t.Errorf("pattern %s: want %t, actual %s", pattern.name, pattern.expectedError, actualError)
 		}
 	}
 }
