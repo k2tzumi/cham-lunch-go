75404603a5932e771e74e0f95800f0ed35ec6146
k2tzumi <katzumi+github@gmail.com>
2022-05-01 21:05:08 +0900

    🏷️ 天気予測のstructを定義

---
+++ workspace/src/jma/forecast.go
@@ -0,0 +1,127 @@
+package jma
+
+import (
+	"encoding/json"
+	"time"
+)
+
+// https://www.jma.go.jp/bosai/forecast/data/forecast/110000.json
+
+type Forecast struct {
+	PublishingOffice string        `json:"publishingOffice"`
+	ReportDatetime   *time.Time    `json:"reportDatetime"`
+	TimeSeries       []*TimeSeries `json:"timeSeries"`
+	TempAverage      *Average      `json:"tempAverage,omitempty"`
+	PrecipAverage    *Average      `json:"precipAverage,omitempty"`
+}
+
+type TimeSeries struct {
+	TimeDefines []*time.Time       `json:"timeDefines"`
+	RawAreas    []*json.RawMessage `json:"areas"`
+}
+
+type AreaWeatherForecast struct {
+	AreaInfo      `json:"area"`
+	WeatherCodes  []string `json:"weatherCodes,omitempty"`
+	Pops          []string `json:"pops,omitempty"`
+	Reliabilities []string `json:"reliabilities,omitempty"`
+}
+
+type AreaTempsForecast struct {
+	AreaInfo      `json:"area"`
+	TempsMin      []string `json:"tempsMin,omitempty"`
+	TempsMinUpper []string `json:"tempsMinUpper,omitempty"`
+	TempsMinLower []string `json:"tempsMinLower,omitempty"`
+	TempsMax      []string `json:"tempsMax,omitempty"`
+	TempsMaxUpper []string `json:"tempsMaxUpper,omitempty"`
+	TempsMaxLower []string `json:"tempsMaxLower,omitempty"`
+}
+
+type AreaInfo struct {
+	AreaName string `json:"name"`
+	AreaCode string `json:"code"`
+}
+
+type Average struct {
+	AreaAverages *AreaAverage `json:"areas"`
+}
+
+type AreaAverage struct {
+	AreaInfo `json:"area"`
+	Min      string `json:"min"`
+	Max      string `json:"max"`
+}
+
+func (f *Forecast) IsWeekly() bool {
+	return f.TempAverage != nil
+}
+
+func (t *TimeSeries) IsWeekly() bool {
+	return len(t.TimeDefines) == 7
+}
+
+func (t *TimeSeries) WeatherAreas() []AreaWeatherForecast {
+	var areas []AreaWeatherForecast
+
+	for _, area := range t.RawAreas {
+		weather := AreaWeatherForecast{}
+
+		if err := json.Unmarshal(*area, &weather); err != nil {
+			return nil
+		}
+		if len(weather.WeatherCodes) == 0 {
+			continue
+		}
+
+		areas = append(areas, weather)
+	}
+
+	return areas
+}
+
+func (t *TimeSeries) TempsAreas() []AreaTempsForecast {
+	var areas []AreaTempsForecast
+
+	for _, area := range t.RawAreas {
+		temps := AreaTempsForecast{}
+
+		if err := json.Unmarshal(*area, &temps); err != nil {
+			return nil
+		}
+		if len(temps.TempsMin) == 0 {
+			continue
+		}
+
+		areas = append(areas, temps)
+	}
+
+	return areas
+}
+
+func (f *Forecast) GetWeeklyWeatherForecast() []AreaWeatherForecast {
+	for _, timeSeries := range f.TimeSeries {
+		if timeSeries.IsWeekly() {
+			weatherAreas := timeSeries.WeatherAreas()
+
+			if weatherAreas != nil {
+				return weatherAreas
+
+			}
+		}
+	}
+
+	return nil
+}
+
+func (f *Forecast) GetWeeklyTempsForecast() []AreaTempsForecast {
+	for _, timeSeries := range f.TimeSeries {
+		if timeSeries.IsWeekly() {
+			tempsAreas := timeSeries.TempsAreas()
+			if tempsAreas != nil {
+				return tempsAreas
+			}
+		}
+	}
+
+	return nil
+}
